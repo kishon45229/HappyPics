dbdf9c00e27db3b873a5d76ef02797335e8481dc
Kishon <76579709+kishon45229@users.noreply.github.com>
2024-07-20 13:51:00 +0530

    Update HappyPics.py

--- HappyPics.py
+++ HappyPics.py
@@ -16,7 +16,13 @@ load_dotenv()
 
 # Firebase configuration
 firebase_config = {
-    
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "databaseURL": os.getenv("DATABASE_URL"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId": os.getenv("APP_ID")
 }
 
 firebase = pyrebase.initialize_app(firebase_config)
